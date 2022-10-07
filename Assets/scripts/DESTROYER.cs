21b560708470ab7294ffbbe8a9a8c108c1ba2c12
KarmaCc <113114243+KarmaCc@users.noreply.github.com>
2022-10-07 12:41:18 +0100

    set up new endless runner

---
+++ Assets/scripts/DESTROYER.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DESTROYER : MonoBehaviour
+{
+    //much like the start () and update() methods, OnTriggerEnter2D is a special Unity method that is called 
+    //by Unity autoumattically at a specific point - in this case, when something enters the Trigger attached
+    //to this GameObject
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the GameObject that has collided with our trigger is tagged with CleanUp...
+        if (collision.gameObject.tag == "CleanUp")
+        {
+            //then we use this method to destroy it
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
