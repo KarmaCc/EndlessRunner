5816e761e5ef781f3d89e55b1123394ebad54ddd
KarmaCc <carmencassiusclay@gmail.com>
2022-10-11 13:34:25 +0100

    polish

--- Assets/scripts/Parallax.cs
+++ Assets/scripts/Parallax.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
-GameObject player;
-Renderer rend;
+public class Parallax : MonoBehaviour 
+{
+    GameObject player;
+    Renderer rend;
 
     float playerStartPos;
     public float speed = 0.7f
-{
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-        rend = GetComponent < Renderer.();
-        playerStartPos
+        rend = GetComponent <Renderer>();
+        //playerStartPos
     }
 
     // Update is called once per frame
